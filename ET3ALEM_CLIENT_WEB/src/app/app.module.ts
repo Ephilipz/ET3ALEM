fd4471f5c84ca0e23c087cf1151fd9dec9106d0a
Eesaa Philips <32400503+Ephilipz@users.noreply.github.com>
2020-04-28 01:47:19 +0200

    removed package lock

--- ET3ALEM_CLIENT_WEB/src/app/app.module.ts
+++ ET3ALEM_CLIENT_WEB/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home/home.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HomeModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
