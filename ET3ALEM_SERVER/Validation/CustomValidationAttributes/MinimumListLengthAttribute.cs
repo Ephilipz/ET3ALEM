fb03460f2f9119754ad7a182c6e1ce504f579483
Ahmed_AdelS_Mohammed <ahmed.adels.mohammed@gmail.com>
2020-06-05 19:09:31 +0200

    add validation for list minimum length

---
+++ ET3ALEM_SERVER/Validation/CustomValidationAttributes/MinimumListLengthAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace Validation.CustomValidationAttributes
+{
+    public class MinimumListLengthAttribute : ValidationAttribute
+    {
+        private readonly int minNumberofElements;
+
+        public MinimumListLengthAttribute(int minNumberofElements)
+        {
+            this.minNumberofElements = minNumberofElements;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            IList list;
+            if (value is IList)
+            {
+                list = value as IList;
+                bool result = list?.Count >= minNumberofElements;
+
+                return result
+                    ? ValidationResult.Success
+                    : new ValidationResult($"{validationContext.DisplayName} requires at least {minNumberofElements} element" + (minNumberofElements > 1 ? "s" : string.Empty));
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
