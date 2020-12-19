6b7d54bb15ea87ea692f92652ea230651166bde8
Ahmed_AdelS_Mohammed <ahmed.adels.mohammed@gmail.com>
2020-12-19 21:06:54 +0200

    get quizzes by user #19

---
+++ ET3ALEM_SERVER/DataAccessLayer/Migrations/20201219185058_addQuizUser.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class addQuizUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Quiz",
+                nullable: false,
+                defaultValue: "",
+                maxLength: 256);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quiz_UserId",
+                table: "Quiz",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Quiz_AspNetUsers_UserId",
+                table: "Quiz",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Quiz_AspNetUsers_UserId",
+                table: "Quiz");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Quiz_UserId",
+                table: "Quiz");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Quiz");
+        }
+    }
+}
