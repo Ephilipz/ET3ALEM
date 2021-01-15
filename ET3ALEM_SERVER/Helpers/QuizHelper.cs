bc646a1830d07dc19377fb1da6b13813b98a4a4e
Ahmed_AdelS_Mohammed <ahmed.adels.mohammed@gmail.com>
2021-01-15 14:56:03 +0200

    rename getCode method

--- ET3ALEM_SERVER/Helpers/QuizHelper.cs
+++ ET3ALEM_SERVER/Helpers/QuizHelper.cs
@@ -8,7 +8,7 @@ namespace Helpers
     public class QuizHelper
     {
         private static readonly string salt = "r8455qRJMx";
-        public static string getCode(int id)
+        public static string GetCode(int id)
         {
             var hashids = new Hashids(salt, 5, "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
             return hashids.Encode(id);
