2a6f5bc3bea4ad6caed5c4e01b540505f71aeb0a
Eesaa Bilal <eesaa2000@gmail.com>
2021-09-02 23:01:10 +0200

    Added contact service TS

---
+++ ET3ALEM_CLIENT_WEB/src/app/General/components/contact/contact.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  private baseUrl = environment.baseUrl + '/api/ContactUs'
+  constructor(private http: HttpClient) {
+  }
+
+  public submitContactForm(formValue: any){
+    return this.http.post<any>(this.baseUrl, formValue);
+  }
+}
